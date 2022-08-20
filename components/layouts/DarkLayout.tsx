f18c6fb6f482ee6b44c28b08a2bc5b742650ea58
Dary Castillo <darycastillo27@gmail.com>
2022-08-20 13:06:50 -0600

    first commit

---
+++ components/layouts/DarkLayout.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+export const DarkLayout = ({ children }: Props) => {
+  return (
+    <div
+      style={{
+        backgroundColor: "rgba(0,0,0,0.3)",
+        borderRadius: "5px",
+        padding: "10px",
+      }}
+    >
+      <h3>Dark-Layout</h3>
+      <div>{children}</div>
+    </div>
+  );
+};
